0b7b10963848dfbfbed7a3ac5165eeb01fcabab9
abdulrhim4 <abdulrhimsaeed@gmail.com>
2024-07-27 09:13:28 -0700

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Create a function def pascal_triangle(n): that returns a list of lists """
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers"""
+    ang = [[1]]
+    for u in range(n - 1):
+        num = [1]
+        for k in range(u):
+            num.append(ang[-1][0 + k] + ang[-1][1 + k])
+        num.append(1)
+        ang.append(num)
+    return (ang)
