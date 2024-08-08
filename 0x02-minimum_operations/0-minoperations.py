ae4245c2085afc779b4d082f0547229de68a2cdd
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-08 16:25:08 +0000

    interview

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -6,11 +6,11 @@ def minOperations(n: int) -> int:
     """execute only two operations in this file"""
     operat = 0
     cop = 2
-    if (n <= 1):
-        return (0)
-    while( n > 1):
-        while(n % cop == 0):
+    if n <= 1:
+        return 0
+    while n > 1:
+        while n % cop == 0:
             operat += cop
             n //= cop
         cop += 1
-    return (operat)
+    return operat
