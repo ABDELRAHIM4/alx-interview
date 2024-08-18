42b212beaaef75923d3ac3e6f38c518a3ce5566e
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-18 15:12:47 -0700

    interview

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,22 +7,25 @@ import sys
 
 total = 0
 stat = {}
+num = 0
 def print_code():
-    print("File size", total)
+    print("File size: {}".format(total))
     for c in sorted(stat.keys()):
-        print(c, ":", stat[c])
+        print(f"{c}: {stat[c]}" )
 try:
-    for u, line in enumerate(sys.stdin):
-        p = line.split()
+    for line in sys.stdin:
+        p = line.strip()
         if len(p) != 10 or p[3] != 'GET' or p[4] != '/projects/260' or p[5] != 'HTTP/1.1':
             continue
         op = int(p[7])
         size = int(p[8])
+        total += size
         if op in stat:
             stat[op] += 1
         else:
             stat[op] = 1
-        if (u + 1) % 10 == 0:
+        num += 1
+        if num % 10 == 0:
             print_code()
 except KeyboardInterrupt:
     print_code()
