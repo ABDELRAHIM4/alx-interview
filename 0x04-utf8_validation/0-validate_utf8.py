8de7b8879a62ee8febe00f1041bf9912987cbef9
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-22 16:09:17 -0700

    interview

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
 """data set represents a valid UTF-8 encoding"""
-import utf8
 
 
 def validUTF8(data):
     """data set represents a valid UTF-8 encoding"""
-    try:
-        utf8.decode(data)
-        return (True)
-    except utf8.DecoderError:
-        return (False)
+    num = 0
+    for byte in data:
+        binary = bin(byte)[2:].zfill(8)
+        if num == 0:
+            if binary[0] == '0':
+                continue
+            if binary[:3] == '110':
+                num = 1
+            elif binary[:4] == '1110':
+                num = 2
+            elif binary[:5] == '11110':
+                num = 3
+            else:
+                return (False)
+        else:
+            if binary[:2] == '10':
+                return (False)
+            num -= 1
+    return (num == 0 )
