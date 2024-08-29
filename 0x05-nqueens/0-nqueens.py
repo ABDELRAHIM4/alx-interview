1604e2f0e4a57f50972b1eb8fe1fc7fd11a39b04
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-08-29 18:31:51 +0000

    interview

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""N queens puzzle is the challenge of placing N"""
+import sys
+
+
+def safe(n, row, col):
+    """N queens puzzle is the challenge of placing N"""
+    for i in range(row):
+        if n[i] == col or\
+            n[i] - 1 == col - row or \
+            n[i] + 1 == col + row:
+                return (False)
+    return (True)
+
+
+def solve(n, row, s):
+    """N queens puzzle is the challenge of placing N"""
+    if row == n:
+        print([[i, col] for i, col in enumerate(s)])
+        return
+    for col in range(n):
+        if safe(s ,row, col):
+            n[row] = col
+            solve(n, row + 1, s)
+
+def main():
+
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
+
+if __name__ == "__main__":
+    main()
