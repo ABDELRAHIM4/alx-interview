1445c1abe14aaa835e999197e08585cc9227789b
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-11-27 08:22:06 -0800

    interview

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""determine the fewest number of coins needed to meet a given amount total"""
+
+
+def makeChange(coins, total):
+    """determine the fewest number of coins needed to meet total"""
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    if total <= 0:
+        return (0)
+    for coin in coins:
+        if coin < 0:
+            return (-1)
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+    return (dp[total] if dp[total] != float('inf') else (-1))
