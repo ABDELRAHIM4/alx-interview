2a51ccbc27cf33ee61b8587327f218a9b0cca817
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-11-27 08:28:21 -0800

    interview

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,10 +9,8 @@ def makeChange(coins, total):
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
     if total <= 0:
-        return (0)
+        return 0
     for coin in coins:
-        if coin < 0:
-            return (-1)
         for x in range(coin, total + 1):
             dp[x] = min(dp[x], dp[x - coin] + 1)
-    return (dp[total] if dp[total] != float('inf') else (-1))
+    return (dp[total] if dp[total] != float('inf') else -1)
