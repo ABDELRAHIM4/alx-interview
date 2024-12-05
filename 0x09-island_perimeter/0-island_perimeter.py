3cd3b5a612cda4286368cf8b15d3363b7ae0015b
ABDELRAHIM4 <abdulrhimsaeed@gmail.com>
2024-12-05 15:58:24 -0800

    interview

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,14 +8,14 @@ def island_perimeter(grid):
     row = len(grid)
     col = len(grid[0]) if row > 0 else 0
     for i in range(row):
-                for j in range(col):
-                        if grid[i][j] == 1:
-                            if i == 0 or grid[i - 1][j] == 0:
-                                count += 1
-                            if j == 0 or grid[i][j - 1] == 0:
-                                count += 1         
-                            if i == row - 1 or grid[i + 1][j] == 0:
-                                count += 1
-                            if j == col - 1 or grid[i][j + 1] == 0: 
-                                count += 1
+        for j in range(col):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    count += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    count += 1
+                if i == row - 1 or grid[i + 1][j] == 0:
+                    count += 1
+                if j == col - 1 or grid[i][j + 1] == 0:
+                    count += 1
     return (count)
